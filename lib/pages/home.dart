f71ca0b57eaed46bbc4c51bd705a5d4486e6f69e
raul <raul@o2w.es>
2020-03-25 23:11:30 +0100

    Añadiendo imagenes prototipo

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'package:eatapp/models/receta.dart';
+import 'package:eatapp/test/data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -9,6 +11,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  void _onPressed(){
+    print("pressed!!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,7 +26,64 @@ class _HomeState extends State<Home> {
             child: ListView(
               scrollDirection: Axis.vertical,
               children: <Widget>[
-                _TopCard(),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: Stack(children: <Widget>[
+                    _TopCard(),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: FractionallySizedBox(
+                        heightFactor: 0.65,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.16),
+                                blurRadius: 6.0, // has the effect of softening the shadow
+                                offset: Offset(
+                                  0.0, // horizontal, move right 10
+                                  -3.0, // vertical, move down 10
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment(0, -0.4),
+                      child: SizedBox(
+                        height: 338.0,
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 250.0,
+                              child: _RecetasList(),
+                              ),
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Row(),
+                                  OutlineButton(
+                                    borderSide: BorderSide(
+
+                                    ),
+                                    onPressed: _onPressed,
+                                    child: Text("Ver todas"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    ],
+                  ),
+                )
             ],),
           ),
         ],
@@ -28,13 +92,15 @@ class _HomeState extends State<Home> {
   }
 }
 
+
+
 class _TopCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
     return Container(
-      height: h * 0.45,
+      height: h * 0.8,
       decoration: BoxDecoration(
         color: Colors.white,
         image: new DecorationImage(
@@ -45,8 +111,8 @@ class _TopCard extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-          begin: Alignment(0, -1.6),
-          end: Alignment(0, 1.4),
+          begin: Alignment(0, -1.3),
+          end: Alignment(0, 0),
           colors: [Colors.deepPurple, const Color.fromARGB(0, 0, 0, 0)], // whitish to gray
         ),
         ),
@@ -59,15 +125,19 @@ class _TopCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Descubrir",
+                    Text("Para hoy",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40),
+                        color: Colors.white),
+                        textAlign: TextAlign.left,
                       ),
-                    Text("Por Platos y Lugares", style: TextStyle(color: Colors.white)),
+                    Text("Pasta Fresca", style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,),
+                      textAlign: TextAlign.left,
+                      ),
                   ]
                 ),
                 CircleAvatar(
@@ -80,6 +150,69 @@ class _TopCard extends StatelessWidget {
                 ),
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RecetasList extends StatelessWidget { 
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int index) {
+        Receta receta = recetas[index % recetas.length];
+        if (index == 0) {
+          return Padding(
+            padding: EdgeInsets.only(left: 70.0),
+            child: _RecetasTile(receta));
+        }
+        return _RecetasTile(receta);
+      }
+    );
+  }
+}
+
+class _RecetasTile extends StatelessWidget {
+  _RecetasTile(this.receta);
+
+  final Receta receta;
+
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        height: 250.0,
+        width: 160.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: Colors.blueGrey,
+          image: new DecorationImage(
+            image: new ExactAssetImage(receta.imgUrl),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child:Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Icon(Icons.favorite, color: Colors.redAccent),
+              Text(receta.titulo, style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ),
       ),
