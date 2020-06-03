7fa7e46c5dcc97c5895c52dc3c2b48fabd7e4254
RaulSangosto <raulsangosto@gmail.com>
2020-06-03 15:26:32 +0200

    descrubrir

--- lib/pages/descubrir_page.dart
+++ lib/pages/descubrir_page.dart
@@ -7,7 +7,11 @@ import 'package:flutter/rendering.dart';
 import 'package:get_it/get_it.dart';
 
 class DescubrirPage extends StatefulWidget {
-  const DescubrirPage({Key key, bool loged, Function(bool) loginCallback, Function(int) pageIdCallback})
+  const DescubrirPage(
+      {Key key,
+      bool loged,
+      Function(bool) loginCallback,
+      Function(int) pageIdCallback})
       : _isLoged = loged,
         _loginCallback = loginCallback,
         _pageIdCallback = pageIdCallback,
@@ -30,19 +34,28 @@ class _DescubrirState extends State<DescubrirPage> {
   List<Categoria> categorias;
   List<Receta> recetas;
   bool _isLoading = false;
+  bool _filterOpen = false;
 
-  _scrollListener(){
-    if(gridScrollController.offset <= 80) {
+  _scrollListener() {
+    if (gridScrollController.offset <= 80) {
       _fetchRecetas();
-      gridScrollController.animateTo(150, duration: Duration(milliseconds: 500), curve: Curves.linear);
+      gridScrollController.animateTo(150,
+          duration: Duration(milliseconds: 500), curve: Curves.linear);
     }
+  }
+
+  _openTopCardCallBack(bool open) {
+    setState(() {
+      _filterOpen = open;
+    });
   }
 
   @override
   void initState() {
     _fetchRecetas();
     super.initState();
-    gridScrollController = new ScrollController(initialScrollOffset: 150.0, keepScrollOffset: true);
+    gridScrollController =
+        new ScrollController(initialScrollOffset: 150, keepScrollOffset: true);
     gridScrollController.addListener(_scrollListener);
   }
 
@@ -62,95 +75,178 @@ class _DescubrirState extends State<DescubrirPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: //Builder(builder: (_) {
-        // if (_isLoading) {
-        //   return Center(child: CircularProgressIndicator());
-        // }
-
-        // if (_apiResponse.error) {
-        //   return Center(child: Text(_apiResponse.errorMessage));
-        // }
-
-        Stack(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                (_isLoading) ? Center(child: CircularProgressIndicator(),) : Expanded(
-                  child: SizedBox(
-                    height: 472.0,
-                    child: _RecetasGrid(recetas: recetas, controller: gridScrollController),
-                  ),
-                ),
-              ],
-            ),
-            _TopCard(),
-          ],
-        ),
+      child: Stack(
+        //mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              (_isLoading)
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: _filterOpen ? 60.0 : 0.0),
+                        child: SizedBox(
+                          height: 472.0,
+                          child: _RecetasGrid(
+                              recetas: recetas,
+                              controller: gridScrollController),
+                        ),
+                      ),
+                    ),
+            ],
+          ),
+          new TopCard(
+            openCallBack: _openTopCardCallBack,
+            categorias: categorias,
+          ),
+        ],
+      ),
       //}
-      );
+    );
     //);
   }
 }
 
-class _TopCard extends StatelessWidget {
+class TopCard extends StatefulWidget {
+  TopCard({this.openCallBack, this.categorias});
+  Function openCallBack;
+  List<Categoria> categorias;
+  
+
+  @override
+  _TopCardState createState() => _TopCardState();
+}
+
+class _TopCardState extends State<TopCard> {
+  bool open = false;
+  int selectedIndex;
+
+  @override
+  void initState() {
+    print(widget.categorias.length);
+    selectedIndex = -1;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 115.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.0)),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromRGBO(0, 0, 0, 0.16),
-            blurRadius: 6.0, // has the effect of softening the shadow
-            offset: Offset(
-              0.0, // horizontal, move right 10
-              3.0, // vertical, move down 10
-            ),
-          )
-        ],
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 10.0,
-          vertical: 20.0,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      "Explorar",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+    return SizedBox(
+      height: open ? 180 : 115.0,
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          Positioned.fill(
+            child: Container(
+              height: 100.0,
+              padding: EdgeInsets.only(top: 50.0),
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30.0)),
+                color: Colors.grey,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.16),
+                    blurRadius: 6.0, // has the effect of softening the shadow
+                    offset: Offset(
+                      0.0, // horizontal, move right 10
+                      3.0, // vertical, move down 10
                     ),
-                    Text("Por Platos y Lugares"),
-                  ]),
-              SizedBox(
-                width: 70.0,
+                  )
+                ],
+              ),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: widget.categorias.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Categoria categoria = widget.categorias[index];
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex =index;
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                            top: 90.0, left: 10.0, right: 10.0),
+                        child: Text(
+                          categoria.titulo,
+                          style: TextStyle(color: (index == selectedIndex) ? Theme.of(context).accentColor : Colors.white),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ),
+          Container(
+            height: 115.0,
+            decoration: BoxDecoration(
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(30.0)),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.16),
+                  blurRadius: 6.0, // has the effect of softening the shadow
+                  offset: Offset(
+                    0.0, // horizontal, move right 10
+                    3.0, // vertical, move down 10
+                  ),
+                )
+              ],
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 20.0,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Icon(
-                      Icons.search,
-                      size: 28.0,
-                    ),
-                    Icon(
-                      Icons.list,
-                      size: 28.0,
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            "Explorar",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 40),
+                          ),
+                          Text("Por Platos y Lugares"),
+                        ]),
+                    SizedBox(
+                      width: 70.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Icon(
+                            Icons.search,
+                            size: 28.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                open = !open;
+                                widget.openCallBack(open);
+                              });
+                            },
+                            child: Icon(
+                              open ? Icons.close : Icons.filter_list,
+                              size: 28.0,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -175,82 +271,16 @@ class _RecetasGrid extends StatelessWidget {
         crossAxisCount: 2,
         childAspectRatio: (3 / 4),
       ),
-      itemBuilder: (BuildContext context, int index) {     
-        if(index < 2){
-          return Container(height: 30, width: 30,);
+      itemBuilder: (BuildContext context, int index) {
+        if (index < 2) {
+          return Container(
+            height: 30,
+            width: 30,
+          );
         }
-        Receta receta = recetas[index -2];
+        Receta receta = recetas[index - 2];
         return RecetaTile(receta, h / 3, (w - 40) / 2);
       },
     );
   }
 }
-
-// class _RecetasTile extends StatelessWidget {
-//   _RecetasTile(this.receta);
-
-//   final Receta receta;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     double w = MediaQuery.of(context).size.width;
-//     double h = MediaQuery.of(context).size.height;
-
-//     return Container(
-//       height: h / 3,
-//       width: (w - 40) / 2,
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(20.0),
-//         color: Colors.blueGrey,
-//         image: new DecorationImage(
-//           image: new NetworkImage(receta.imgUrl),
-//           fit: BoxFit.cover,
-//         ),
-//       ),
-//       child: Padding(
-//         padding: const EdgeInsets.all(15.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           crossAxisAlignment: CrossAxisAlignment.end,
-//           children: <Widget>[
-//             Icon(Icons.favorite, color: Colors.redAccent),
-//             Text(
-//               receta.titulo,
-//               style: TextStyle(
-//                 fontSize: 18.0,
-//                 fontWeight: FontWeight.w600,
-//                 color: Colors.white,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class _RecetasGrid extends StatelessWidget {
-//    @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 10.0),
-//       child: new StaggeredGridView.countBuilder(
-//         scrollDirection: Axis.vertical,
-//         crossAxisCount: 4,
-//         itemCount: 20,
-//         itemBuilder: (BuildContext context, int index) => new Container(
-//             color: Colors.green,
-//             child: new Center(
-//               child: new CircleAvatar(
-//                 backgroundColor: Colors.white,
-//                 child: new Text('$index'),
-//               ),
-//             )),
-//         staggeredTileBuilder: (int index) =>
-//             new StaggeredTile.count(2, index.isEven ? 2 : 1),
-//         mainAxisSpacing: 10.0,
-//         crossAxisSpacing: 10.0,
-//       ),
-//     );
-//   }
-// }
